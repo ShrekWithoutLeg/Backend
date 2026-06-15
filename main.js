bce5ff050c9636abd19872ae4d1463af84e757b3
ShrekWithoutLeg <tryother124@gmail.com>
2026-06-15 15:59:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ import { Server } from "socket.io";
 
 const app = express();
 app.use(express.json());
-const redis = new Redis({ url: process.env.REDIS_URL });
+const redis = new Redis(process.env.REDIS_URL);
 const port = process.env.PORT || 4000;
 
 app.use(
